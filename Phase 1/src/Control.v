4b7672a1d1dbfa0b371ad1a5f534d836b02e255d
Abhimanyu Kulkarni <43687621+AbhimanyuKulkarni@users.noreply.github.com>
2018-10-12 18:55:14 -0500

    Add files via upload

---
+++ Phase 1/src/Control.v
@@ -0,0 +1,23 @@
+module Control(input[3:0] Op, 
+				input rst_n,
+				output RegRead, RegWrite, MemRead, MemWrite, halt,
+				output[1:0] ALUSrc, WriteSelect, Branch,
+				output[3:0] ALUOp);
+				
+assign RegRead = Op[3]&Op[1];
+assign RegWrite = (~Op[3])|(~Op[2] & ~Op[0])|(~Op[2] & Op[1])|(Op[1] & ~Op[0]);
+assign ALUSrc[0] = (Op[2] & ~Op[1])|(Op[2] & ~Op[0])|(Op[3] & Op[1]);
+assign ALUSrc[1] = Op[3];
+assign ALUOp[0] = (~Op[3] & Op[0])|(Op[1] & Op[0]);
+assign ALUOp[1] = ~Op[3] & Op[1];
+assign ALUOp[2] = Op[2];
+assign ALUOp[3] = Op[3] & Op[1];
+assign MemRead = ~Op[0];
+assign halt = Op[3]&Op[2]&Op[1]&Op[0];
+assign Branch[0] = Op[3]&Op[2]&~Op[1]&Op[0];
+assign Branch[1] = Op[3]&Op[2]&~Op[1];
+assign WriteSelect[0] = Op[3]&~Op[1];
+assign WriteSelect[1] = Op[3]&Op[2];
+assign MemWrite = Op[3]&~Op[2]&~Op[1]&Op[0];
+
+endmodule
