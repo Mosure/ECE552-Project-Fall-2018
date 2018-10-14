5a7c00a1598e8c423ef5cdd6f158cbe84664b800
Mitchell Mosure <hctimit@gmail.com>
2018-10-14 16:26:29 -0500

    Added assembler to test script

--- Phase 1/build.bat
+++ Phase 1/build.bat
@@ -3,34 +3,43 @@
 echo Building shifter...
 iverilog -o ./bin/shifter ./src/shifter.v
 iverilog -o ./bin/test/shifter_tb ./src/shifter.v ./src/test/shifter_tb.v
+echo.
 
 echo Building sign extender...
 iverilog -o ./bin/sign_ext ./src/shifter.v ./src/sign_ext.v
 iverilog -o ./bin/test/sign_ext_tb ./src/shifter.v ./src/sign_ext.v ./src/test/sign_ext_tb.v
+echo.
 
 echo Building ALU...
 iverilog -o ./bin/alu ./src/shifter.v ./src/alu.v
 iverilog -o ./bin/test/alu_tb ./src/shifter.v ./src/alu.v ./src/test/alu_testbench.v
+echo.
 
 echo Building Register File...
 iverilog -o ./bin/register_file ./src/ReadDecoder_4_16.v ./src/WriteDecoder_4_16.v ./src/D-Flip-Flop.v ./src/BitCell.v ./src/Register.v ./src/RegisterFile.v
+echo.
 
 echo Building Memory...
 iverilog -o ./bin/memory ./src/memory.v
+echo.
 
 echo Building Control...
 iverilog -o ./bin/control ./src/Control.v
+echo.
 
 echo Building PC Control...
 iverilog -o ./bin/pc_control ./src/PC_control.v
 iverilog -o ./bin/test/pc_control_tb ./src/PC_control.v ./src/test/PC_Control_tb.v
+echo.
 
 echo Building CPU...
 iverilog -o ./bin/cpu ./src/ReadDecoder_4_16.v ./src/WriteDecoder_4_16.v ./src/D-Flip-Flop.v ./src/BitCell.v ./src/Register.v^
             ./src/Control.v ./src/PC_control.v ./src/RegisterFile.v ./src/shifter.v ./src/alu.v ./src/memory.v ./src/cpu.v
+echo.
 
 echo Building Phase 1 Testbench...
-iverilog -o ./bin/test/phase1_tb ./src/ReadDecoder_4_16.v ./src/WriteDecoder_4_16.v ./src/D-Flip-Flop.v ./src/BitCell.v ./src/Register.v^
+iverilog -o ./project-phase1-testbench/phase1_tb ./src/ReadDecoder_4_16.v ./src/WriteDecoder_4_16.v ./src/D-Flip-Flop.v ./src/BitCell.v ./src/Register.v^
             ./src/Control.v ./src/PC_control.v ./src/RegisterFile.v ./src/shifter.v ./src/alu.v ./src/memory.v ./src/cpu.v ./project-phase1-testbench/project-phase1-testbench.v
+echo.
 
 echo Done.
--- Phase 1/test.bat
+++ Phase 1/test.bat
@@ -2,4 +2,26 @@
 for %%f in (./bin/test/*) do (
     echo "Running: %%f"
     vvp ./bin/test/%%f
+    echo.
 )
+
+echo.
+echo Running test1.list...
+perl ../WISC-assembler/assembler.pl ./project-phase1-testcases/test1.list > ./project-phase1-testcases/loadfile_all.img
+vvp ./project-phase1-testbench/phase1_tb
+type "./project-phase1-testbench/verilogsim.trace"
+type "./project-phase1-testbench/verilogsim.log"
+
+echo.
+echo Running test2.list...
+perl ../WISC-assembler/assembler.pl ./project-phase1-testcases/test2.list > ./project-phase1-testcases/loadfile_all.img
+vvp ./project-phase1-testbench/phase1_tb
+type "./project-phase1-testbench/verilogsim.trace"
+type "./project-phase1-testbench/verilogsim.log"
+
+echo.
+echo Running test3.list...
+perl ../WISC-assembler/assembler.pl ./project-phase1-testcases/test3.list > ./project-phase1-testcases/loadfile_all.img
+vvp ./project-phase1-testbench/phase1_tb
+type "./project-phase1-testbench/verilogsim.trace"
+type "./project-phase1-testbench/verilogsim.log"
