53d9c829b1b698a79c4f8f66e2bc0b95531b8535
hfrentzel <44076377+hfrentzel@users.noreply.github.com>
2018-10-16 14:53:24 -0500

    Update Control.v
    Add flag register enables

--- Phase 1/src/Control.v
+++ Phase 1/src/Control.v
@@ -1,5 +1,5 @@
 module Control(input[3:0] Op,
-		output RegRead, RegWrite, MemWrite, halt,
+		output RegRead, RegWrite, MemWrite, halt, zEn, vEn, nEn,
 		output[1:0] ALUSrc, WriteSelect, Branch,
 		output[3:0] ALUOp);
 				
@@ -17,5 +17,8 @@ assign Branch[1] = Op[3] & Op[2] & ~Op[1];
 assign WriteSelect[0] = Op[3] & ~Op[1];
 assign WriteSelect[1] = Op[3] & Op[2];
 assign MemWrite = Op[3] & ~Op[2] & ~Op[1] & Op[0];
+assign zEn = ~Op[3];
+assign vEn = ~|Op[3:1];
+assign nEn = ~|Op[3:1];
 
 endmodule
