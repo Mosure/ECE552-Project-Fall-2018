fb41e1f470b672f9eaed54c229e90e6041804c33
Abhimanyu Kulkarni <43687621+AbhimanyuKulkarni@users.noreply.github.com>
2018-10-17 17:59:16 -0500

    Update Control.v

--- Phase 1/src/Control.v
+++ Phase 1/src/Control.v
@@ -1,7 +1,9 @@
-module Control(input[3:0] Op,
-		output RegRead, RegWrite, MemWrite, halt, zEn, vEn, nEn,
-		output[1:0] ALUSrc, WriteSelect, Branch,
-		output[3:0] ALUOp);
+module Control(Op, RegRead, RegWrite, MemWrite, halt, zEn, vEn, nEn, ALUSrc, WriteSelect, Branch, ALUOp);
+			   
+input[3:0] Op;
+output RegRead, RegWrite, MemWrite, halt, zEn, vEn, nEn;
+output[1:0] ALUSrc, WriteSelect, Branch;
+output[3:0] ALUOp;
 				
 assign RegRead = Op[3] & Op[1];
 assign RegWrite = (~Op[3])|(~Op[2] & ~Op[0])|(~Op[2] & Op[1])|(Op[1] & ~Op[0]);
