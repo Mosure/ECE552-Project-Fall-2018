744c12a6dd4c11599676eced28c251b2ce2e781b
hfrentzel <44076377+hfrentzel@users.noreply.github.com>
2018-11-12 16:31:34 -0600

    Added Hazard Detection for changing flags

--- Phase 2/src/Hazard_Detection.v
+++ Phase 2/src/Hazard_Detection.v
@@ -1,11 +1,13 @@
-module Hazard_Detection(X_memRead, D_memWrite, X_Rt, D_Rs, D_Rt, stall);
+module Hazard_Detection(X_memRead, D_memWrite, X_Rt, D_Rs, D_Rt, stall, Opcode, CC);
 
 input X_memRead, D_memWrite;
 input[3:0] X_Rt, D_Rs, D_Rt;
+input[3:0] opcode;
+input[2:0] CC;
 
 output stall;
 
-wire Op1_dep, Op2_dep;
+wire Op1_dep, Op2_dep Flag_dep;
 
 //// Op1_dep: Load to use dependency on 1st operand i.e. X_Rt = D_Rs ////
 assign Op1_dep = (X_memRead) && (X_Rt != 4'h0) && (X_Rt == D_Rs);
@@ -15,7 +17,12 @@ assign Op1_dep = (X_memRead) && (X_Rt != 4'h0) && (X_Rt == D_Rs);
 
 assign Op2_dep = (X_memRead) && (X_Rt != 4'h0) && (X_Rt == D_Rs) && (~D_memWrite);
 
+
+//// Flag_dep: Stall is needed when conditional branch exists,
+//// As flags need to be updated. No stall for conditional brances
+assign Flag_dep = (opcode[3] & opcode[2] & ~opcode[1] & ~(&CC));
+
 //// Enable stall in either of the two cases ////
-assign stall = Op1_dep | Op2_dep;
+assign stall = Op1_dep | Op2_dep | Flag_dep;
 
 endmodule
--- Phase 2/src/cpu.v
+++ Phase 2/src/cpu.v
@@ -25,13 +25,13 @@ module cpu (clk, rst_n, hlt, pc);
     wire[3:0] D_ALUOp, D_Rs, D_Rt, D_Rd;
     wire[15:0] D_regData1, D_regData2;
     wire stall;
-	
+
     // EX Phase signals
-	wire[1:0] forwardA, forwardB;
-	wire MMforward;
+    wire[1:0] forwardA, forwardB;
+    wire MMforward;
     wire[2:0] Flag, FlagIn;
     wire[15:0] ALUSrcMuxOut;
-	wire[15:0] ALUSrc1, ALUSrc2;
+    wire[15:0] ALUSrc1, ALUSrc2;
     wire[15:0] X_offset, X_shamt, X_loadByte;
     wire X_regWrite, X_memRead, X_memWrite, X_zEn, X_vEn, X_nEn, X_hlt;
     wire[1:0] X_writeSelect, X_ALUsrc;
@@ -39,7 +39,7 @@ module cpu (clk, rst_n, hlt, pc);
     wire[15:0] X_regData1, X_regData2, X_incPC, X_ALUOut;
     
     // MEM Phase signals
-	wire[15:0] DMEM_In;
+    wire[15:0] DMEM_In;
     wire M_regWrite, M_memWrite, M_hlt;
     wire[1:0] M_writeSelect;
     wire[3:0] M_Rs, M_Rt, M_Rd;
@@ -52,7 +52,7 @@ module cpu (clk, rst_n, hlt, pc);
     wire[3:0] W_Rs, W_Rt, W_Rd;
     wire[15:0] W_DMemOut, W_incPC, W_ALUOut;
 
-	    
+    
     // Convert active low reset to active high reset
     assign rst = ~rst_n;
 
@@ -89,17 +89,17 @@ module cpu (clk, rst_n, hlt, pc);
     // 16 register Register File
     RegisterFile registers(.clk(clk), .rst(rst), .SrcReg1(D_Rs), .SrcReg2(D_Rt), .DstReg(D_instruction[11:8]),
                             .WriteReg(W_RegWrite), .DstData(DstData), .SrcData1(D_regData1), .SrcData2(D_regData2));
-	
+
     // Global Control Logic
     Control GlobalControl(.Op(D_instruction[15:12]), .RegRead(RegRead), .RegWrite(W_regWrite), .MemRead(D_memRead), .MemWrite(D_memWrite), .halt(D_hlt), .ALUSrc(D_ALUsrc),
                             .Branch(Branch), .WriteSelect(D_writeSelect), .ALUOp(D_ALUOp), .zEn(D_zEn), .vEn(D_vEn), .nEn(D_nEn));
                             
     // PC Control Logic
     BranchControl BC(.C(D_instruction[11:9]), .I(D_instruction[8:0]), .F(Flag), .B(Branch), .regPC(D_regData1), .incPC(D_incPC), .branchPC(branchPC), .exBranch(exBranch));
-	
-	// Hazard Detection Unit
-	Hazard_Detection HDU(.X_memRead(X_memRead), .D_memWrite(D_memWrite), .X_Rt(X_Rt), .D_Rs(D_Rs), .D_Rt(D_Rt), .stall(stall));
-	
+
+    // Hazard Detection Unit
+    Hazard_Detection HDU(.X_memRead(X_memRead), .D_memWrite(D_memWrite), .X_Rt(X_Rt), .D_Rs(D_Rs), .D_Rt(D_Rt), .stall(stall), .opcode(D_instruction[15_12], .CC(D_instruction[11:9]);
+
     // determine all the possible immediate inputs to ALU
     assign D_offset = {{11{D_instruction[3]}}, D_instruction[3:0], 1'b0};
     assign D_shamt  = {12'h000, D_instruction[3:0]};
@@ -118,27 +118,26 @@ module cpu (clk, rst_n, hlt, pc);
 	
     // ALUsrc MUX
     assign ALUSrcMuxOut = (X_ALUsrc == 2'b00) ? X_regData2 :   	 // register Data
-						  (X_ALUsrc == 2'b01) ? X_shamt :        // zero extended immediate
-						  (X_ALUsrc == 2'b10) ? X_offset :       // offset for LW or SW
-												X_loadByte;      // 8-bit immediate
-	
-	// Forwarding Unit
-	Forwarding_Unit FwdUnit(.M_regWrite(M_regWrite), .W_regWrite(W_RegWrite), .M_memWrite(M_memWrite), .M_Rd(M_Rd), .W_Rd(W_Rd),
-							.X_Rs(X_Rs), .X_Rt(X_Rt), .M_Rt(M_Rt), .forwardA(forwardA), .forwardB(forwardB), .MMforward(MMforward));
-	
-	// Forwarding MUXes
+                          (X_ALUsrc == 2'b01) ? X_shamt :        // zero extended immediate
+                          (X_ALUsrc == 2'b10) ? X_offset :       // offset for LW or SW
+                                                X_loadByte;      // 8-bit immediate
 
-	assign ALUSrc1 = (forwardA == 2'b01) ? M_ALUOut :			 // EX-EX forwarding
-					 (forwardA == 2'b10) ? DstData :			 // MEM-EX forwarding
-										   X_regData1;			 // No forwarding
+    // Forwarding Unit
+    Forwarding_Unit FwdUnit(.M_regWrite(M_regWrite), .W_regWrite(W_RegWrite), .M_memWrite(M_memWrite), .M_Rd(M_Rd), .W_Rd(W_Rd),
+                            .X_Rs(X_Rs), .X_Rt(X_Rt), .M_Rt(M_Rt), .forwardA(forwardA), .forwardB(forwardB), .MMforward(MMforward));
 
-	assign ALUSrc2 = (forwardB == 2'b01) ? M_ALUOut :			 // EX-EX forwarding
-					 (forwardB == 2'b10) ? DstData :			 // MEM-EX forwarding
-										   ALUSrcMuxOut;		 // No forwarding										   
+    // Forwarding MUXes
+    assign ALUSrc1 = (forwardA == 2'b01) ? M_ALUOut :			 // EX-EX forwarding
+                     (forwardA == 2'b10) ? DstData :			 // MEM-EX forwarding
+                                           X_regData1;			 // No forwarding
+
+    assign ALUSrc2 = (forwardB == 2'b01) ? M_ALUOut :			 // EX-EX forwarding
+                     (forwardB == 2'b10) ? DstData :			 // MEM-EX forwarding
+                                           ALUSrcMuxOut;		 // No forwarding										   
     
-	// ALU
+    // ALU
     alu iALU(.op1(ALUSrc1), .op2(ALUSrc2), .aluop(X_ALUOp), .Flag(FlagIn), .alu_out(X_ALUOut));
-	
+
     // 3 flip-flops, one for each flag
     // Flag Order is ZVN
     FlagRegisters flags(.clk(clk), .rst(rst), .FlagIn(FlagIn), .zEn(X_zEn), .vEn(X_vEn), .nEn(X_nEn), .FlagOut(Flag));
@@ -155,10 +154,10 @@ module cpu (clk, rst_n, hlt, pc);
     ** Memory Access Stage **
     ************************/
     
-	// MEM_MEM Forwarding MUX
-	assign DMEM_In = MMforward ? DstData : M_regData2; 
-	
-	// Data Memory
+    // MEM_MEM Forwarding MUX
+    assign DMEM_In = MMforward ? DstData : M_regData2; 
+
+    // Data Memory
     memory1c DMEM(.clk(clk), .rst(rst), .data_in(DMEM_In), .addr(M_ALUOut), .enable(1'b1), .wr(M_MemWrite), .data_out(M_DMemOut));
     
     // MEM/WB pipeline register
