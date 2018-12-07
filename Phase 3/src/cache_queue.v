97e62782ed5e70aa781129d6bf7dd98ce1134bc1
Mitchell Mosure <hctimit@gmail.com>
2018-12-07 12:08:43 -0600

    finished cache queue

--- Phase 3/src/cache_queue.v
+++ Phase 3/src/cache_queue.v
@@ -32,14 +32,28 @@ module cache_queue(clk, rst, iMemValid, iMemDataOut, iMemDataIn, iMemAddress, iM
     wire            data_out;
 
     // Handle states here
+    wire            state;        // 1 if reading
+    wire            next_state;
+    wire            current_read; // 0 if IMEM, 1 if DMEM
 
+    dff stateFlop(.clk(clk), .rst(rst), .d(next_state), .q(state), .wen(1'b1));
 
-    memory4c MEM (.data_out(data_out), .data_in(dMemDataIn), .addr(address), .enable(enable), .wr(mem_write), .clk(clk), .rst(rst), .data_valid(data_valid));
+    assign next_state = (state) ? ((data_valid) ? 1'b0 : 1'b1) : ((iMemEnable | dMemEnable) ? 1'b1 : 1'b0);
+
+    assign enable = (mem_write) ? 1'b1 : (~state & (iMemEnable | dMemEnable));
+
+    assign current_read = (state) ? current_read : dMemEnable;
+
+    assign address = (mem_write) ? dMemAddress : ((current_read) ? dMemAddress : iMemAddress);
+
+    // Ground state -> Read1 or Read2
+
+    memory4c MEM (.data_out(data_out), .data_in(dMemDataIn), .addr(address), .enable(enable), .wr(dMemWrite), .clk(clk), .rst(rst), .data_valid(data_valid));
 
     assign iMemDataOut = data_out;
     assign dMemDataOut = data_out;
 
     // assign valid based on state and memory.data_valid
-    assign iMemValid = 1'b0;
-    assign dMemValid = 1'b0;
+    assign iMemValid = state & data_valid & ~current_read;
+    assign dMemValid = state & data_valid & current_read;
 endmodule
